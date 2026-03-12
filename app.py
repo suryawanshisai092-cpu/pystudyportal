c1b701688dadc7e10613e48e1f1b08bba20812c0
codewithsai <suryawanshisai092@gmail.com>
2026-03-12 20:47:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from db import get_connection
 from flask import Flask, request, jsonify, send_from_directory
 from flask import request, jsonify
 from flask_cors import CORS
-
+from flask_mail import Mail, Message
+import random
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -161,6 +163,7 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
