9cae29f1a90a329caf84a2ac60c079ca207b6c58
codewithsai <suryawanshisai092@gmail.com>
2026-03-16 22:59:58 +0530

    Delete static directory

--- static/app.js
+++
@@ -1,8 +0,0 @@
-// service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/static/service-worker.js")
-    .then(() => console.log("Service Worker Registered"));
-}
-
-
-// LOGIN FUNCTION
--- static/service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker Installed");
-});
-
-self.addEventListener("fetch", function(event) {
-});
